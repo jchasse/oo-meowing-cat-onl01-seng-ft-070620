47c4fbaf9fe45cdcb018fd48de65fa75be6fbc85
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-13 03:52:01 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,16 @@
-## code your solution here. 
+## code your solution here.
+
+class Cat 
+  def initilize(name)
+    @name = name
+  end
+
+  attr_accessor = :name 
+
+def meow
+  puts "Meow"
+end
+
+  
+end
+
